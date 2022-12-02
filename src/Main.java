99718a096a116bfb4eea2e5748253dd7a4f1379d
niieli <96092663+niieli@users.noreply.github.com>
2022-12-02 01:58:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Taschenrechner");
+        char operator;
+        Double number1, number2, result;
+        
+    }
+}
