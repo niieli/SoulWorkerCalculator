08cac246b468637144a89fe85f1f37c3f8d0ec56
niieli <96092663+niieli@users.noreply.github.com>
2023-01-01 04:15:15 +0100

    Continue

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,40 @@ class Main {
 
         char operator;
 
-        double number1, number2, number3, gear, result, temp;
+        double number1, number2, number3, cry, result, temp;
         Scanner input = new Scanner(System.in);
 
         //TF oder BS Gear berechnen
-        System.out.println("Choose your desired gear." + "\nEither press 1 for Tragic Fairy or 2 for Broken Savior");
+        System.out.println("Tragic Fairy only. More coming soon!");
+        System.out.println("""
+                Choose your desired need.
+                Type '1': Tragic Fairy full gear
+                Type '2': Tragic Fairy full gear without accessories
+                Type '3': Tragic Fairy Weapon only
+                Type '4': Tragic Fairy accessories""");
         operator = input.next().charAt(0);
 
-        //Was genau gebraucht wird
-        System.out.println("Do you need full gear?\nYes('1') or No('0')?");
-        gear = input.nextDouble();
+        System.out.println("Do you need a custom amount of 'Crystals'(1) or Default(0)?");
+        cry = input.nextDouble();
 
-        //Anzahl der Crystals
-        if(gear==0){
+        if(cry==1){
             System.out.println("How many 'Crystals' do you need?");
             number1 = input.nextDouble();
-        } else {
+        } else if(operator=='1'){
             number1 = 240;
+        } else if (operator=='2'){
+            number1 = 144;
+        } else if (operator=='3'){
+            number1 = 32;
+        } else if (operator=='4'){
+            number1 = 96;
+        } else {
+            number1 = 0;
+            System.err.println("Something went wrong");
         }
 
         //Avg. Zeit pro Run
-        if(!(operator == 0)){
-            System.out.println("What is your average time to complete 'Field of Raid' in Seconds?");
-        } else {
-            System.out.println("What is your average time to complete 'Blooming Road EP4' in Seconds?");
-        }
+        System.out.println("What is your average time to complete 'Field of Raid' in Seconds?");
         number2 = input.nextDouble();
 
 
@@ -39,22 +48,45 @@ class Main {
 
         //Noch mal neu bedenken was für Features nützlich sein könnten
         switch (operator) {
-            //Tragic Fairy
-            case '1':
-                //Anzahl pieces, Runs
+            //Tragic Fairy full gear
+            case '1' -> {
+                //Berechnen
                 number1 = number1 * 10;
                 temp = number1 / number3;
                 result = temp * number2;
                 result = result / 60;
-                System.out.println("Required pieces: "+ number1 + "\nRequired Runs: " + temp + " with an average of '" + number3 + "' drops per run");
+                System.out.println("Required crystals: " + number1 / 10 + "\nRequired pieces: " + number1 + "\nRequired Runs: " + temp + " with an average of '" + number3 + "' drops per run");
                 System.out.println("Required time to farm: " + result + " minutes, with an average of '" + number2 + "' seconds per run");
-                if(gear==1){
-                    System.out.println("Required powder: 498(Flower) & 960(Citrus)");
-                }
-                break;
-
-            case '2':
-                break;
+                System.out.println("Required powder: 498(Flower) & 960(Citrus)");
+            }
+            case '2' -> {
+                //Berechnen
+                number1 = number1 * 10;
+                temp = number1 / number3;
+                result = temp * number2;
+                result = result / 60;
+                System.out.println("Required crystals: " + number1 / 10 + "\nRequired pieces: " + number1 + "\nRequired Runs: " + temp + " with an average of '" + number3 + "' drops per run");
+                System.out.println("Required time to farm: " + result + " minutes, with an average of '" + number2 + "' seconds per run");
+                System.out.println("Required powder: 210(Flower) & 480(Citrus)");
+            }
+            case '3' -> {
+                number1 = number1 * 10;
+                temp = number1 / number3;
+                result = temp * number2;
+                result = result / 60;
+                System.out.println("Required crystals: " + number1 / 10 + "\nRequired pieces: " + number1 + "\nRequired Runs: " + temp + " with an average of '" + number3 + "' drops per run");
+                System.out.println("Required time to farm: " + result + " minutes, with an average of '" + number2 + "' seconds per run");
+                System.out.println("Required powder: 210(Flower) & 0(Citrus)");
+            }
+            case '4' -> {
+                number1 = number1 * 10;
+                temp = number1 / number3;
+                result = temp * number2;
+                result = result / 60;
+                System.out.println("Required crystals: " + number1 / 10 + "\nRequired pieces: " + number1 + "\nRequired Runs: " + temp + " with an average of '" + number3 + "' drops per run");
+                System.out.println("Required time to farm: " + result + " minutes, with an average of '" + number2 + "' seconds per run");
+                System.out.println("Required powder: 288(Flower) & 480(Citrus)");
+            }
         }
     }
 }
