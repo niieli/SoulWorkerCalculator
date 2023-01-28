98d0bde3c80a82111b87653e0e53c9f552b6fd0a
niieli <96092663+niieli@users.noreply.github.com>
2023-01-28 14:27:27 +0100

    Removed colour

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 import java.util.Scanner;
 
 class Main {
-    public static final String RESET = "\u001B[0m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLACK = "\u001B[30m";
-    public static final String WHITE = "\u001B[37m";
-    public static final String BLACK_BACKGROUND = "\u001B[40m";
-    public static final String WHITE_BACKGROUND = "\u001B[47m";
-    public static final String YELLOW_BACKGROUND = "\u001B[43m";
     public static void main(String[] args) {
 
         char operator;
 
-        int number1, number2, number3, cry, seconds, runs, zenny, cost, hours, minutes, sec;
+        int number1, number2, number3, cry, seconds, runs, zenny, cost, hours, minutes, sec, number7, runs2;
         Scanner input = new Scanner(System.in);
 
         //TF oder BS Gear berechnen
@@ -24,11 +17,12 @@ class Main {
                 Type '2': Tragic Fairy full gear without accessories
                 Type '3': Tragic Fairy Weapon only
                 Type '4': Tragic Fairy Accessories
-                Type '5': Tragic Fairy Gear only\n"""+ YELLOW_BACKGROUND + BLACK +"""
-                Type '6': Dawn of Fate farming"""+ RESET);
+                Type '5': Tragic Fairy Gear only
+                Type '6': Dawn of Fate farming
+                """);
         operator = input.next().charAt(0);
 
-        if (operator!='6' && operator!='0') {
+        if (operator!='6' && operator!='0' && operator!='7') {
             System.out.println("\nDo you need a custom amount of 'Crystals'(1) or Default(0)?");
             cry = input.nextInt();
         } else {
@@ -48,7 +42,7 @@ class Main {
             number1 = 96;
         } else if (operator=='5') {
             number1 = 112;
-        } else if (operator=='6' || operator=='0') {
+        } else if (operator=='6' || operator=='0' || operator=='7') {
                 System.out.println("\nHow much Zenny do you need?");
                 number1 = input.nextInt();
         } else {
@@ -58,17 +52,24 @@ class Main {
         }
 
         //Avg. Zeit pro Run
-        if (operator!='6' && operator!='0') {
+        if (operator!='6' && operator!='0' && operator!='7') {
             System.out.println("\nWhat is your average time to complete 'Field of Raid' in Seconds?");
         } else if (operator=='6'){
             System.out.println("\nWhat is your average time to complete 'Dawn of Fate' in Seconds?");
         } else {
-            System.out.println("\nWhat is your average time to complete 'Garden of Plenty' in Seconds");
+            System.out.println("\nWhat is your average time left in 'Garden of Plenty' in Seconds?");
         }
         number2 = input.nextInt();
 
+        if(operator=='7') {
+            System.out.println("\nWhat is your average time to complete 'Dawn of Fate' in Seconds?");
+            number7 = input.nextInt();
+        } else {
+            number7 = 0;
+        }
+
         //Avg. Droprate
-        if (operator!='6' && operator!='0') {
+        if (operator!='6' && operator!='0' && operator!='7') {
             System.out.println("\nHow many pieces do you expect per run?");
             number3 = input.nextInt();
         } else {
@@ -175,12 +176,31 @@ class Main {
                     runs = runs + 1;
                     zenny = runs * 1137838;
                 }
+                number2 = 120 - number2;
                 int exp = 20 * runs;
                 seconds = runs * number2;
                 sec = seconds % 60;
                 minutes = seconds % 3600 / 60;
                 hours = seconds % 86400 / 3600;
                 System.out.println("Required Runs: " + runs + " with an average of '" + number2 + "' seconds per run");
+                System.out.println("Required time to farm: " + hours + " hour(s), " + minutes + " minutes and " + sec + " seconds");
+                System.out.println("End result: " + zenny + " zenny and " + exp + " spirit exp");
+            }
+            case '7' -> {
+                //Moorland of Solitude: 165435 Zenny
+                //Need to implement DoF and kind of priority queue
+                runs2 = number1 / 165435;
+                runs = number1 / 1137838;
+                zenny = (runs * 1137838) + (runs2 * 165435);
+                int exp = 20 * runs;
+                int exp2 = 50 * runs2;
+                exp = exp + exp2;
+                seconds = runs * number2;
+                sec = seconds % 60;
+                minutes = seconds % 3600 / 60;
+                hours = seconds % 86400 / 3600;
+                System.out.println("Required Runs(Garden of Plenty): " + runs + " with an average of '" + number2 + "' seconds per run");
+                System.out.println("Required Runs(Moorland of Solitude): " + runs2 + " with an average of '" + number7 + "' seconds per run");
                 System.out.println("Required time to farm: " + hours + " hour(s), " + minutes + " minutes and " + sec + " seconds");
                 System.out.println("End result: " + zenny + " zenny and " + exp + " spirit exp");
             }
