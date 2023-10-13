f7048365ad3f3ab51a7d235ebd478cfecb843f64
niieli <96092663+niieli@users.noreply.github.com>
2023-10-13 20:05:52 +0200

    Vile Predator Support

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 class Main {
@@ -5,7 +6,8 @@ class Main {
 
         char operator;
 
-        int number1, number2, number3, cry, seconds, runs, zenny, cost, hours, minutes, sec, number7, runs2;
+        int number1, number2, number3, cry, seconds, runs, zenny, cost, hours, minutes, sec, number7, runs2, wep, armor, accs;
+        String snumber1, sruns, scost, szenny;
         Scanner input = new Scanner(System.in);
 
         //TF oder BS Gear berechnen
@@ -19,8 +21,22 @@ class Main {
                 Type '4': Tragic Fairy Accessories
                 Type '5': Tragic Fairy Gear only
                 Type '6': Dawn of Fate farming
+                Type 'X': Vile Predator Gear + Accs
                 """);
         operator = input.next().charAt(0);
+
+        if(operator=='X'){
+            System.out.println("\nDo you want to craft the weapon? | 1 = Yes 0 = No (You can also type '3' if you want 3 Weapons)");
+            wep = input.nextInt();
+        } else {wep=0;}
+        if(operator=='X'){
+            System.out.println("\nHow many armor parts do you want to craft? | (e.g type '3' for 3 armor parts)");
+            armor = input.nextInt();
+        } else {armor=0;}
+        if(operator=='X'){
+            System.out.println("\nHow many accessories do you want to craft? | (e.g type '3' for 3 accessories)");
+            accs = input.nextInt();
+        } else {accs=0;}
 
         if (operator!='6' && operator!='0' && operator!='7') {
             System.out.println("\nDo you need a custom amount of 'Crystals'(1) or Default(0)?");
@@ -42,6 +58,8 @@ class Main {
             number1 = 96;
         } else if (operator=='5') {
             number1 = 112;
+        } else if (operator=='X'){
+            number1 = (40*wep)+(30*(armor+accs));
         } else if (operator=='6' || operator=='0' || operator=='7') {
                 System.out.println("\nHow much Zenny do you need?");
                 number1 = input.nextInt();
@@ -52,10 +70,12 @@ class Main {
         }
 
         //Avg. Zeit pro Run
-        if (operator!='6' && operator!='0' && operator!='7') {
+        if (operator!='6' && operator!='0' && operator!='7' && operator!='X') {
             System.out.println("\nWhat is your average time to complete 'Field of Raid' in Seconds?");
         } else if (operator=='6'){
             System.out.println("\nWhat is your average time to complete 'Dawn of Fate' in Seconds?");
+        } else if (operator=='X'){
+            System.out.println("\nWhat is your average time to complete 'Endless Wasteland' in Seconds?");
         } else {
             System.out.println("\nWhat is your average time left in 'Garden of Plenty' in Seconds?");
         }
@@ -203,6 +223,28 @@ class Main {
                 System.out.println("Required Runs(Moorland of Solitude): " + runs2 + " with an average of '" + number7 + "' seconds per run");
                 System.out.println("Required time to farm: " + hours + " hour(s), " + minutes + " minutes and " + sec + " seconds");
                 System.out.println("End result: " + zenny + " zenny and " + exp + " spirit exp");
+            }
+            case 'X' -> {
+                cost = number1 * 560000 + (11000000*(armor+wep))+(9000000*accs);
+                number1 = number1 * 10;
+                runs = number1 / number3;
+                seconds = runs * number2;
+                sec = seconds % 60;
+                minutes = seconds % 3600 / 60;
+                hours = seconds % 86400 / 3600;
+                zenny = (int)Math.floor(Math.random()*(155000-110000+1)+110000);
+                zenny = zenny * runs;
+                int ampoule = (480*wep)+(120*armor)+(60*accs);
+                int cells = (20*wep)+(10*armor)+(40*accs);
+                snumber1 = NumberFormat.getIntegerInstance().format(number1);
+                String snumber11 = NumberFormat.getIntegerInstance().format(number1/10);
+                sruns = NumberFormat.getIntegerInstance().format(runs);
+                scost = NumberFormat.getIntegerInstance().format(cost);
+                szenny = NumberFormat.getIntegerInstance().format(zenny);
+                String sampoule = NumberFormat.getIntegerInstance().format(ampoule);
+                System.out.println("Required Fused Crystals of Corruption: " + snumber11 + "\nRequired Molten Shards of Corruption: " + snumber1 + "\nRequired Runs: " + sruns + " with an average of '" + number3 + "' drops per run");
+                System.out.println("Required materials: "+sampoule+"(Mutant Blood Ampoule) & "+cells+"(Vacuum Vein Stem Cells)\nRequired Zenny for crafting(Crystals + Gear): "+scost+ " Zenny");
+                System.out.println("Required time to farm: " + hours + " hour(s), " + minutes+" minutes and " + sec+" seconds with an average of '" + number2 + "' seconds per run"+"\nApproximate Zenny gain(Maze clear): " + szenny+" Zenny");
             }
         }
     }
